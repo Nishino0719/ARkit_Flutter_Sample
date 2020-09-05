6602d78e45f01abe7563960eeef5a3a4d27772cd
Nishino0719 <nishino0719@icloud.com>
2020-09-05 11:10:14 +0900

    add Earth sample(not perfectly)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+
 import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart';
 
 void main() => runApp(MaterialApp(home: MyApp()));
@@ -11,22 +13,48 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ARKitController arkitController;
+  Timer timer;
 
   @override
   void dispose() {
+    timer?.cancel();
     arkitController?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+    appBar: AppBar(title: const Text('Earth Sample')),
+    body: Container(
+      child: ARKitSceneView(
+        onARKitViewCreated: onARKitViewCreated,
+      ),
+    ),
+  );
 
   void onARKitViewCreated(ARKitController arkitController) {
     this.arkitController = arkitController;
+
+    final material = ARKitMaterial(
+      lightingModelName: ARKitLightingModel.lambert,
+      diffuse: ARKitMaterialProperty(image: 'images/eye.png'),
+    );
+    final sphere = ARKitSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+
     final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
+      geometry: sphere,
+      position: Vector3(0, 0, -0.5),
+      eulerAngles: Vector3.zero(),
+    );
     this.arkitController.add(node);
+
+    // timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+    //   final rotation = node.eulerAngles;
+    //   rotation.x += 0.01;s
+    //   node.eulerAngles = rotation;
+    // });
   }
 }
