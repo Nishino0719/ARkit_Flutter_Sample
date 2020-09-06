4aff3b569f27764b341c41489368d56bc5b32a10
Nishino0719 <nishino0719@icloud.com>
2020-09-06 22:00:13 +0900

    add Plane Detection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:math' as math;
 import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
@@ -11,6 +12,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ARKitController arkitController;
+  ARKitPlane plane;
+  ARKitNode node;
+  String anchorId;
 
   @override
   void dispose() {
@@ -20,13 +24,58 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+    appBar: AppBar(title: const Text('Plane Detection Sample')),
+    body: Container(
+      child: ARKitSceneView(
+        showFeaturePoints: true,
+        planeDetection: ARPlaneDetection.horizontal,
+        onARKitViewCreated: onARKitViewCreated,
+      ),
+    ),
+  );
 
   void onARKitViewCreated(ARKitController arkitController) {
     this.arkitController = arkitController;
-    final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
-    this.arkitController.add(node);
+    this.arkitController.onAddNodeForAnchor = _handleAddAnchor;
+    this.arkitController.onUpdateNodeForAnchor = _handleUpdateAnchor;
+  }
+
+  void _handleAddAnchor(ARKitAnchor anchor) {
+    if (!(anchor is ARKitPlaneAnchor)) {
+      return;
+    }
+    _addPlane(arkitController, anchor);
+  }
+
+  void _handleUpdateAnchor(ARKitAnchor anchor) {
+    if (anchor.identifier != anchorId) {
+      return;
+    }
+    final ARKitPlaneAnchor planeAnchor = anchor;
+    node.position.value =
+        vector.Vector3(planeAnchor.center.x, 0, planeAnchor.center.z);
+    plane.width.value = planeAnchor.extent.x;
+    plane.height.value = planeAnchor.extent.z;
+  }
+
+  void _addPlane(ARKitController controller, ARKitPlaneAnchor anchor) {
+    anchorId = anchor.identifier;
+    plane = ARKitPlane(
+      width: anchor.extent.x,
+      height: anchor.extent.z,
+      materials: [
+        ARKitMaterial(
+          transparency: 0.5,
+          diffuse: ARKitMaterialProperty(color: Colors.white),
+        )
+      ],
+    );
+
+    node = ARKitNode(
+      geometry: plane,
+      position: vector.Vector3(anchor.center.x, 0, anchor.center.z),
+      rotation: vector.Vector4(1, 0, 0, -math.pi / 2),
+    );
+    controller.add(node, parentNodeName: anchor.nodeName);
   }
 }
