da6143ed7bdf8e2e49c75ff6ec5db1eba2c9396e
Nishino0719 <nishino0719@icloud.com>
2020-09-07 22:55:25 +0900

    add Image Detection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+
 import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
@@ -11,22 +13,76 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ARKitController arkitController;
+  Timer timer;
+  bool anchorWasFound = false;
 
   @override
   void dispose() {
+    timer?.cancel();
     arkitController?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+    appBar: AppBar(title: const Text('Image Detection Sample')),
+    body: Container(
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          ARKitSceneView(
+            detectionImagesGroupName: 'AR Resources',
+            onARKitViewCreated: onARKitViewCreated,
+          ),
+          anchorWasFound
+              ? Container()
+              : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Point the camera at the earth image from the article about Earth on Wikipedia.',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  .copyWith(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 
   void onARKitViewCreated(ARKitController arkitController) {
     this.arkitController = arkitController;
-    final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
-    this.arkitController.add(node);
+    this.arkitController.onAddNodeForAnchor = onAnchorWasFound;
+  }
+
+  void onAnchorWasFound(ARKitAnchor anchor) {
+    if (anchor is ARKitImageAnchor) {
+      setState(() => anchorWasFound = true);
+
+      final material = ARKitMaterial(
+        lightingModelName: ARKitLightingModel.lambert,
+        diffuse: ARKitMaterialProperty(image: 'images/earth.jpg'),
+      );
+      final sphere = ARKitSphere(
+        materials: [material],
+        radius: 0.1,
+      );
+
+      final earthPosition = anchor.transform.getColumn(3);
+      final node = ARKitNode(
+        geometry: sphere,
+        position:
+        vector.Vector3(earthPosition.x, earthPosition.y, earthPosition.z),
+        eulerAngles: vector.Vector3.zero(),
+      );
+      arkitController.add(node);
+
+      timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+        final old = node.eulerAngles;
+        final eulerAngles = vector.Vector3(old.value.x, old.value.y + 0.01, old.value.z);
+        node.eulerAngles.value = eulerAngles;
+      });
+    }
   }
 }
