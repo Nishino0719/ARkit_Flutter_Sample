62e067781c3e1dbb096396a1bc1844fcfe12da5e
Nishino0719 <nishino0719@icloud.com>
2020-09-09 07:40:59 +0900

    add light estimate

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+
 import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
@@ -9,24 +11,61 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+
 class _MyAppState extends State<MyApp> {
   ARKitController arkitController;
+  Timer timer;
 
   @override
   void dispose() {
+    timer?.cancel();
     arkitController?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+    appBar: AppBar(title: const Text('Light Estimation Sample')),
+    body: Container(
+      child: ARKitSceneView(
+        onARKitViewCreated: onARKitViewCreated,
+      ),
+    ),
+  );
 
   void onARKitViewCreated(ARKitController arkitController) {
     this.arkitController = arkitController;
+
+    final material = ARKitMaterial(
+      diffuse: ARKitMaterialProperty(image: 'images/mars.jpg'),
+    );
+    final sphere = ARKitSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+
     final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
+      geometry: sphere,
+      position: vector.Vector3(0, 0, -0.5),
+      eulerAngles: vector.Vector3.zero(),
+    );
     this.arkitController.add(node);
+
+    final light = ARKitLight();
+    final lightNode =
+    ARKitNode(light: light, position: vector.Vector3(0.1, 0.4, 0.5));
+    this.arkitController.add(lightNode);
+
+    timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      final old = node.eulerAngles;
+      final eulerAngles = vector.Vector3(old.value.x, old.value.y+0.1, old.value.z);
+      node.eulerAngles.value = eulerAngles;
+
+      this.arkitController.getLightEstimate().then((e) {
+        if (e != null) {
+          light.intensity.value = e.ambientIntensity;
+        }
+      });
+    });
   }
 }
