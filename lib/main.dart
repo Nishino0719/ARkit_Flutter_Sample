fa1fa6fc0c304ef72b4c24ad4546f4acacf2b548
Nishino0719 <nishino0719@icloud.com>
2020-09-08 09:01:02 +0900

    add custom light

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
@@ -20,13 +20,86 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+      appBar: AppBar(
+        title: const Text('Custom Light Sample'),
+      ),
+      body: Container(
+        child: ARKitSceneView(
+          autoenablesDefaultLighting: false,
+          onARKitViewCreated: onARKitViewCreated,
+        ),
+      ));
 
   void onARKitViewCreated(ARKitController arkitController) {
     this.arkitController = arkitController;
+
+    _addLight(this.arkitController);
+    _addSphere(this.arkitController);
+    _addPlane(this.arkitController);
+    _addText(this.arkitController);
+  }
+
+  void _addLight(ARKitController controller) {
+    final light = ARKitLight(
+      type: ARKitLightType.spot,
+      color: Colors.blueGrey,
+    );
+    final node =
+    ARKitNode(light: light, position: vector.Vector3(0.1, 0.4, 0.5));
+    controller.add(node);
+  }
+
+  void _addSphere(ARKitController controller) {
+    final material = ARKitMaterial(
+      lightingModelName: ARKitLightingModel.physicallyBased,
+      diffuse: ARKitMaterialProperty(
+        color: Colors.red,
+      ),
+    );
+    final sphere = ARKitSphere(
+      materials: [material],
+      radius: 0.1,
+    );
     final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
-    this.arkitController.add(node);
+      geometry: sphere,
+      position: vector.Vector3(0, 0, -0.5),
+    );
+    controller.add(node);
+  }
+
+  void _addPlane(ARKitController controller) {
+    final plane = ARKitPlane(
+      width: 1,
+      height: 1,
+      materials: [
+        ARKitMaterial(
+          transparency: 0.5,
+          diffuse: ARKitMaterialProperty(color: Colors.white),
+        )
+      ],
+    );
+    final node = ARKitNode(
+      geometry: plane,
+      position: vector.Vector3(0.4, 0.4, -1.5),
+    );
+    controller.add(node);
+  }
+
+  void _addText(ARKitController controller) {
+    final text = ARKitText(
+      text: 'Flutter',
+      extrusionDepth: 1,
+      materials: [
+        ARKitMaterial(
+          diffuse: ARKitMaterialProperty(color: Colors.blue),
+        )
+      ],
+    );
+    final node = ARKitNode(
+      geometry: text,
+      position: vector.Vector3(0, 0, -0.5),
+      scale: vector.Vector3(0.02, 0.02, 0.02),
+    );
+    controller.add(node);
   }
 }
