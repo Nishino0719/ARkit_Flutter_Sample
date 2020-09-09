fae95798191e42ae530e8bef395d1e94ed9f98ba
Nishino0719 <nishino0719@icloud.com>
2020-09-09 21:25:03 +0900

    add facetracking but do not run

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
@@ -11,6 +11,13 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ARKitController arkitController;
+  ARKitNode node;
+  String anchorId;
+  ARKitNode leftEye;
+  ARKitNode rightEye;
+  String person = '';
+
+  var children;
 
   @override
   void dispose() {
@@ -19,14 +26,98 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
-
-  void onARKitViewCreated(ARKitController arkitController) {
-    this.arkitController = arkitController;
-    final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
-    this.arkitController.add(node);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Face Detection Sample')),
+      body: Stack(children: [
+        ARKitSceneView(
+          configuration: ARKitConfiguration.faceTracking,
+          onARKitViewCreated: (c) => onARKitViewCreated(c),
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Text(
+            person,
+            style: Theme
+                .of(context)
+                .textTheme
+                .headline1
+                .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        )
+      ]),
+    );
   }
+    void onARKitViewCreated(ARKitController arKitController) {
+      this.arkitController = arKitController;
+      // this.c.onAddNodeForAnchor = _handleAddAnchor;
+      this.arkitController.onUpdateNodeForAnchor = _onUpdateNodeForAnchor;
+    }
+
+    // void _handleAddAnchor(ARKitAnchor anchor) {
+    //   if (!(anchor is ARKitFaceAnchor)) {
+    //     return;
+    //   }
+    //   final material = ARKitMaterial(fillMode: ARKitFillMode.lines);
+    //   final ARKitFaceAnchor faceAnchor = anchor;
+    //   faceAnchor.geometry.materials.value = [material];
+
+    // anchorId = anchor.identifier;
+    // node = ARKitNode(geometry: faceAnchor.geometry);
+    // arkitController.add(node, parentNodeName: anchor.nodeName);
+    //
+    // leftEye = _createEye(faceAnchor.leftEyeTransform);
+    // arkitController.add(leftEye, parentNodeName: anchor.nodeName);
+    // rightEye = _createEye(faceAnchor.rightEyeTransform);
+    // arkitController.add(rightEye, parentNodeName: anchor.nodeName);
+  // }
+
+  // ARKitNode _createEye(Matrix4 transform) {
+  //   final position = vector.Vector3(
+  //     transform
+  //         .getColumn(3)
+  //         .x,
+  //     transform
+  //         .getColumn(3)
+  //         .y,
+  //     transform
+  //         .getColumn(3)
+  //         .z,
+  //   );
+  //   final material = ARKitMaterial(
+  //     diffuse: ARKitMaterialProperty(color: Colors.yellow),
+  //   );
+  //   final sphere = ARKitBox(
+  //       materials: [material], width: 0.03, height: 0.03, length: 0.03);
+  //
+  //   return ARKitNode(geometry: sphere, position: position);
+  // }
+
+  void _onUpdateNodeForAnchor(ARKitAnchor anchor) {
+    if (anchor is ARKitFaceAnchor) {
+      final ARKitFaceAnchor faceAnchor = anchor;
+      final smile = faceAnchor.blendShapes['mouthSmile_L'];
+      if (smile > 0.5) {
+        //happy
+
+        setState(() => person = 'Happiness');
+      } else if (smile < 0.00001) {
+        //sad
+        setState(() => person = 'sad');
+      } else {
+        //nothing
+        setState(() => person = '');
+      }
+      // arkitController.updateFaceGeometry(node, anchor.identifier);
+      // _updateEye(leftEye, faceAnchor.leftEyeTransform,
+      //     faceAnchor.blendShapes['eyeBlink_L']);
+      // _updateEye(rightEye, faceAnchor.rightEyeTransform,
+      //     faceAnchor.blendShapes['eyeBlink_R']);
+    }
+  }
+
+// void _updateEye(ARKitNode node, Matrix4 transform, double blink) {
+//   final scale = vector.Vector3(1, 1 - blink, 1);
+//   node.scale.value = scale;
+// }
 }
