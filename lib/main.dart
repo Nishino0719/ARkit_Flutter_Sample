f1f1e709da746aed025d0cd6575f4dc765b82396
Nishino0719 <nishino0719@icloud.com>
2020-09-11 09:05:06 +0900

    add Muniplation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:math' as math;
 import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
@@ -11,6 +12,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ARKitController arkitController;
+  ARKitNode boxNode;
 
   @override
   void dispose() {
@@ -20,13 +22,75 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+    appBar: AppBar(title: const Text('Manipulation Sample')),
+    body: Container(
+      child: ARKitSceneView(
+        enablePinchRecognizer: true,
+        enablePanRecognizer: true,
+        enableRotationRecognizer: true,
+        onARKitViewCreated: onARKitViewCreated,
+      ),
+    ),
+  );
 
   void onARKitViewCreated(ARKitController arkitController) {
     this.arkitController = arkitController;
+    this.arkitController.onNodePinch = (pinch) => _onPinchHandler(pinch);
+    this.arkitController.onNodePan = (pan) => _onPanHandler(pan);
+    this.arkitController.onNodeRotation =
+        (rotation) => _onRotationHandler(rotation);
+    addNode();
+  }
+
+  void addNode() {
+    final material = ARKitMaterial(
+      lightingModelName: ARKitLightingModel.physicallyBased,
+      diffuse: ARKitMaterialProperty(
+        color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+            .withOpacity(1.0),
+      ),
+    );
+    final box =
+    ARKitBox(materials: [material], width: 0.1, height: 0.1, length: 0.1);
+
     final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
-    this.arkitController.add(node);
+      geometry: box,
+      position: vector.Vector3(0, 0, -0.5),
+    );
+    arkitController.add(node);
+    boxNode = node;
+  }
+
+  void _onPinchHandler(List<ARKitNodePinchResult> pinch) {
+    final pinchNode = pinch.firstWhere(
+          (e) => e.nodeName == boxNode.name,
+      orElse: () => null,
+    );
+    if (pinchNode != null) {
+      final scale = vector.Vector3.all(pinchNode.scale);
+      boxNode.scale.value = scale;
+    }
+  }
+
+  void _onPanHandler(List<ARKitNodePanResult> pan) {
+    final panNode =
+    pan.firstWhere((e) => e.nodeName == boxNode.name, orElse: null);
+    if (panNode != null) {
+      final old = boxNode.eulerAngles;
+      final newAngleY = panNode.translation.x * math.pi / 180;
+      boxNode.eulerAngles.value = vector.Vector3(old.value.x, newAngleY, old.value.z);
+    }
+  }
+
+  void _onRotationHandler(List<ARKitNodeRotationResult> rotation) {
+    final rotationNode = rotation.firstWhere(
+          (e) => e.nodeName == boxNode.name,
+      orElse: () => null,
+    );
+    if (rotationNode != null) {
+      final rotation =
+          boxNode.eulerAngles.value + vector.Vector3.all(rotationNode.rotation);
+      boxNode.eulerAngles.value = rotation;
+    }
   }
 }
